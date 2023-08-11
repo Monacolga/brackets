0844abad9c250ca51d918bd045dacf3eaafbe8b5
Olga Pedashenko <olga.pedashenko@gmail.com>
2023-08-12 00:03:33 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsObject = bracketsConfig.reduce((obj, [key, value]) => {
+    obj[key] = value;
+    return obj;
+  }, {});
+  const leftBrackets = bracketsConfig.map((pair) => pair[0]);
+  const rightBrackets = bracketsConfig.map((pair) => pair[1]);
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      leftBrackets.includes(str[i]) &&
+      str[i] !== "|" &&
+      str[i] !== "7" &&
+      str[i] !== "8"
+    ) {
+      stack.push(str[i]);
+    } else if (
+      rightBrackets.includes(str[i]) &&
+      str[i] !== "|" &&
+      str[i] !== "7" &&
+      str[i] !== "8"
+    ) {
+      if (bracketsObject[stack.pop()] !== str[i]) {
+        return false;
+      }
+    } else if (str[i] === "|" || str[i] === "7" || str[i] === "8") {
+      if (stack.length === 0 || stack[stack.length - 1] !== str[i]) {
+        stack.push(str[i]);
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return !stack.length;
+};
